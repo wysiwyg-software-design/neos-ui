864cc8f406935ab343948f74baf96a1e7dba752b
Christian Müller <christian@flownative.com>
2018-11-22 15:18:52 +0100

    Export all editors and helpers in index.js

---
+++ packages/neos-ui-editors/src/index.js
@@ -0,0 +1,90 @@
+import {
+    TextField,
+    TextArea,
+    Boolean,
+    DateTime,
+    Image,
+    SelectBox,
+    Link,
+    Reference,
+    References,
+    NodeType,
+    CodeMirror,
+    MasterPlugin,
+    AssetEditor,
+    PluginViews,
+    PluginView
+} from './Editors/index';
+import EditorEnvelope from './EditorEnvelope/index';
+
+import {
+    CodeMirrorWrap,
+    ImageCropper,
+    MediaDetailsScreen,
+    MediaSelectionScreen
+} from './SecondaryEditors/index';
+
+import AssetUpload from './Library/AssetUpload';
+import AssetOption from './Library/AssetOption';
+
+import LinkInput from './Library/LinkInput';
+import LinkOption from './Library/LinkOption';
+
+import NodeOption from './Library/NodeOption';
+
+
+export {
+    TextField,
+    TextArea,
+    Boolean,
+    DateTime,
+    Image,
+    SelectBox,
+    Link,
+    Reference,
+    References,
+    NodeType,
+    CodeMirror,
+    MasterPlugin,
+    AssetEditor,
+    PluginViews,
+    PluginView,
+    EditorEnvelope,
+    CodeMirrorWrap,
+    ImageCropper,
+    MediaDetailsScreen,
+    MediaSelectionScreen,
+    AssetUpload,
+    AssetOption,
+    LinkInput,
+    LinkOption,
+    NodeOption
+};
+
+export default {
+    TextField,
+    TextArea,
+    Boolean,
+    DateTime,
+    Image,
+    SelectBox,
+    Link,
+    Reference,
+    References,
+    NodeType,
+    CodeMirror,
+    MasterPlugin,
+    AssetEditor,
+    PluginViews,
+    PluginView,
+    EditorEnvelope,
+    CodeMirrorWrap,
+    ImageCropper,
+    MediaDetailsScreen,
+    MediaSelectionScreen,
+    AssetUpload,
+    AssetOption,
+    LinkInput,
+    LinkOption,
+    NodeOption
+};
